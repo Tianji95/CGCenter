cec01f9f17dc633b5f33e77e461554a4c2c31622
Tianji95 <441867138@qq.com>
2021-08-15 18:04:46 +0800

    add assimp

--- src/runtime/render.h
+++ src/runtime/render.h
@@ -1,6 +1,9 @@
 #pragma once
 #ifndef RENDER_H
 #define RENDER_H
+#include <assimp/include/cimport.h>
+#include <assimp/include/scene.h>
+#include <assimp/include/postprocess.h>
 #include "camera.h"
 #include "shader.h"
 class Render {
@@ -18,6 +21,7 @@ private:
 	unsigned int VBO = 0;
 	unsigned int VAO = 0;
 	unsigned int colorbuffer = 0;
+	const aiScene* scene = NULL;
 };
 
 #endif // !RENDER_H
